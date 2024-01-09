accf720c821715cab7bdee4c6da64988a62cd0df
MizuSARiUs <thomas.lebaron@etu.univ-tours.fr>
2024-01-09 15:18:56 +0100

    Verification de CEVAvionVeutAtterrir

--- ProjetsSimulation/CEVAvionVeutAtterrir.cpp
+++ ProjetsSimulation/CEVAvionVeutAtterrir.cpp
@@ -2,24 +2,21 @@
 #include "CEVAvionAtterrit.h"
 CEVAvionVeutAtterrir::CEVAvionVeutAtterrir()
 {
-	pisteAtterissage = vector<CPisteAtterissage*>();
+	pisteAtterissage = vector<CPisteAtterissage>();
 	avion = new CAvion();
 }
 CEVAvionVeutAtterrir::CEVAvionVeutAtterrir(CAvion p_avion, vector<CPisteAtterissage> p_pistes, time_t p_temps) {
 	avion = new CAvion(p_avion);
-	pisteAtterissage = vector<CPisteAtterissage*>();
+	pisteAtterissage = vector<CPisteAtterissage>();
 	pisteAtterissage.resize(p_pistes.size());
 	for (int i = 0; i < pisteAtterissage.size(); i++) {
-		*pisteAtterissage[i] = p_pistes[i];
+		pisteAtterissage[i] = p_pistes[i];
 	}
 	this->ecrireTempsDebut(p_temps);
 }
 CEVAvionVeutAtterrir::~CEVAvionVeutAtterrir()
 {
 	if (pisteAtterissage.size() != 0) {
-		for(int i = 0; i<pisteAtterissage.size();i++){
-			delete(pisteAtterissage[i]);
-		}
 		pisteAtterissage.clear();
 	}
 	if (avion != NULL) {
@@ -28,17 +25,22 @@ CEVAvionVeutAtterrir::~CEVAvionVeutAtterrir()
 
 }
 void CEVAvionVeutAtterrir::run() {
+	std::cout << "Avion veut atterrir\n";
 	for (int i = 0; i < pisteAtterissage.size(); i++) {
-		if (!pisteAtterissage[i]->lireOccupation() && pisteAtterissage[i]->lireListeAttenteAvion().empty()) {
-			pisteAtterissage[i]->modifierOccupation(true);
-			CEVAvionAtterrit EVAA(*avion, *pisteAtterissage[i], this->lireTempsDebut());
+		if (!pisteAtterissage[i].lireOccupation() && pisteAtterissage[i].lireListeAttenteAvion().empty()) {
+			pisteAtterissage[i].modifierOccupation(true);
+			CEVAvionAtterrit EVAA(*avion, pisteAtterissage[i], this->lireTempsDebut());
 			EVAA.run();
+			std::cout << "Avion"; std::cout << avion->lireIdAvion(); std::cout << " va atterrir";
+
 		}
-		else if (!pisteAtterissage[i]->lireListeAttenteAvion().empty()) {
-			pisteAtterissage[i]->modifierOccupation(true);
-			CEVAvionAtterrit EVAA(*(pisteAtterissage[i]->lireListeAttenteAvion().front()), *pisteAtterissage[i], this->lireTempsDebut());
-			pisteAtterissage[i]->retirerAvionListeA();
+		else if (!pisteAtterissage[i].lireOccupation()) {
+			pisteAtterissage[i].modifierOccupation(true);
+			CEVAvionAtterrit EVAA(*(pisteAtterissage[i].lireListeAttenteAvion().front()), pisteAtterissage[i], this->lireTempsDebut());
+			int n = pisteAtterissage[i].lireListeAttenteAvion().front()->lireIdAvion();
+			pisteAtterissage[i].retirerAvionListeA();
 			EVAA.run();
+			std::cout << "Avion "; std::cout << n; std::cout << " va atterrir a "; std::cout << this->lireTempsDebut();
 		}
 
 	}
--- ProjetsSimulation/CEVAvionVeutDebarquer.cpp
+++ ProjetsSimulation/CEVAvionVeutDebarquer.cpp
@@ -9,6 +9,7 @@ CEVAvionVeutDebarquer::CEVAvionVeutDebarquer()
 CEVAvionVeutDebarquer::CEVAvionVeutDebarquer(CAvion p_avion, vector<CPorteEmbarquement> p_gates, time_t p_temps)
 {
 	avion = new CAvion(p_avion);
+	avion->modifierEtat(avion->stringToEnum("ATTENTE_PORTE"));
 	gates.resize(p_gates.size());
 	for (int i = 0; i < gates.size(); i++) {
 		*gates[i] = p_gates[i];
@@ -28,4 +29,16 @@ CEVAvionVeutDebarquer::~CEVAvionVeutDebarquer()
 void CEVAvionVeutDebarquer::run()
 {
 	
+	for (int i = 0; i < gates.size() && avion->enumToString(avion->lireEtat()) == "ATTENTE_PORTE"; i++) {
+		//Liste attente vide, on peut faire débarquer l'avion
+		// ! Voir si possibilité de !
+		if (gates[i]->lireListeAttenteAvion().empty() && !gates[i]->lireOccupation()) {
+			
+		}
+		//Liste attente non vide, on fait décoller le premier avion de la queue
+		else if (!gates[i]->lireListeAttenteAvion().empty()) {
+
+		}
+		//La liste d'attente est actualisée à la sortie de chaque avion
+	}
 }
